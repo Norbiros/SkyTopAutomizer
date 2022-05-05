a81a12de9d4fc706647844f39b7b773c52a7feac
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-05-05 17:33:21 +0000

    IDEA is useful :)

--- src/main/java/dev/norbiros/skytopautomizer/functions/FastMath.java
+++ src/main/java/dev/norbiros/skytopautomizer/functions/FastMath.java
@@ -1,14 +1,26 @@
 package dev.norbiros.skytopautomizer.functions;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class FastMath {
+  
   @SubscribeEvent
   public void onClientChat(ClientChatReceivedEvent event) {
-    String message = (event.getMessage().getUnformattedText());
-    if (message.matches("hej")) {
+    if (event.getMessage().matches("hej")) {
       getLocalPlayer().sendChatMessage("Reply");
+      event.setCanceled(true);
     }
   }
 }
