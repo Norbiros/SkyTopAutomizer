3523cab13a9859d989078ca64334789e1f39a6be
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-05-24 12:46:10 +0000

    New AntyCaps system!

--- src/main/java/dev/norbiros/skytopautomizer/functions/FastMath.java
+++ src/main/java/dev/norbiros/skytopautomizer/functions/FastMath.java
@@ -1,26 +1,48 @@
 package dev.norbiros.skytopautomizer.functions;
+import dev.norbiros.skytopautomizer.SkytopAutomizer;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.World;
+
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Mod.EventBusSubscriber(modid = SkytopAutomizer.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class FastMath {
   
   @SubscribeEvent
-  public void onClientChat(ClientChatReceivedEvent event) {
-    if (event.getMessage().matches("hej")) {
-      getLocalPlayer().sendChatMessage("Reply");
-      event.setCanceled(true);
+  public static void onClientChat(ClientChatReceivedEvent event) {
+
+    Pattern pattern = Pattern.compile("^[♂♀]?[ ]?\\[(.*?)\\] (.*?):[ ]?(.*)");
+    Matcher matcher = pattern.matcher( event.getMessage().getString() );
+    
+    if (matcher.find()) {
+      String substring = "uwu";
+      if (event.getMessage().getString().contains(substring)) {
+        Minecraft.getInstance().player.chat("/a Ktoś napisał UWU na chat <3");
+      }
+    
+      String chatMessage = matcher.group(3);
+		  int chatMessageLength = chatMessage.replace(" ", "").length();
+		  int capsLetterAmount = 0;
+		  for (int i = 0; i < chatMessage.length(); i++) {
+        char letter = chatMessage.charAt(i);
+        if (letter >= 65 && letter <= 90) {
+          capsLetterAmount = capsLetterAmount + 1;
+        }
+      }
+    
+      if (capsLetterAmount >= 5 && ((double)capsLetterAmount / (double)chatMessageLength) >= 0.5) ) {
+        Minecraft.getInstance().player.chat("Uważaj na capslock!");
+      }
+      
     }
   }
 }
