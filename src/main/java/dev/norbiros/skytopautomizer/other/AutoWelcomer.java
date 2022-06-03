3497ab68931af0d11507cc7056a5961818377f35
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-06-03 21:33:06 +0200

    fix bugs

--- src/main/java/dev/norbiros/skytopautomizer/other/AutoWelcomer.java
+++ src/main/java/dev/norbiros/skytopautomizer/other/AutoWelcomer.java
@@ -8,18 +8,18 @@ public class AutoWelcomer {
   public static void handleMessage(String nickName) {
     Random rand = new Random();
     int time = rand.nextInt(200);
-    time = (time + 500) * 10;
+    time = (time + 300) * 10;
     try {
       Thread.sleep(time);
     } catch(InterruptedException ex) {
       System.out.println("[SKYTOPAUTOMIZER] Unexpected error: " + ex);
     }
-    if (rand.nextInt(2) == 0) {
+    if (rand.nextInt(4) != 0) {
       switch (rand.nextInt(4)) {
         case 0 -> sendMessage("Hej " + nickName);
         case 1 -> sendMessage("Czesc " + nickName);
         case 2 -> sendMessage("Witaj " + nickName);
-        default -> sendMessage("hi " + nickName);
+        default -> sendMessage("hej " + nickName);
       }
     }
   }
