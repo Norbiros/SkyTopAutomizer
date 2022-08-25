5035fe8e2ca304f0b5845bf1aaae336d5d156ac7
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-08-25 20:46:04 +0200

    Update ChatHandler.java

--- src/main/java/dev/norbiros/skytopautomizer/administration/ChatHandler.java
+++ src/main/java/dev/norbiros/skytopautomizer/administration/ChatHandler.java
@@ -22,62 +22,9 @@ import net.minecraft.client.multiplayer.PlayerInfo;
 
 @Mod.EventBusSubscriber(modid = SkytopAutomizer.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ChatHandler {
-    
   @SubscribeEvent
   public static void onClientChat(ClientChatReceivedEvent event) {
-
-    final Pattern pattern = Pattern.compile("[♀♂? ]?[ ]?(\\[(.*?)\\])?( )?(.*?):[ ]?(.*)");
-    final Pattern nickPattern = Pattern.compile("(\\[(.*?)\\])?( )?([^ ]*)[ ]?[♀♂? ]?[ AFK]?");
-    final Pattern privateMessagePattern = Pattern.compile("PW (.*?) -> (.*?): (.*)");
-    final Pattern welcomePattern = Pattern.compile("Przywitaj nowego gracza (.*) aby");
-    Matcher matcher = pattern.matcher( event.getMessage().getString() );
-    Matcher privateMessageMatcher = privateMessagePattern.matcher( event.getMessage().getString() );
-    Matcher welcomeMatcher = welcomePattern.matcher( event.getMessage().getString() );
-    
-    boolean userIsOnServer = Minecraft.getInstance().getCurrentServer().ip == "skytop.pl";
-
-    if (privateMessageMatcher.find()) {
-      if (privateMessageMatcher.group(2).equalsIgnoreCase("Ja")) {
-        System.out.println("[SKYTOPAUTOMIZER] Trying to handle Admin bot message!" );
-        if (PrivateAdminBot.handleMessage(privateMessageMatcher.group(1), privateMessageMatcher.group(3)) == 1) {
-          event.setCanceled(true);
-        }
-      } else if (privateMessageMatcher.group(3).contains("[AB]")) {
-        System.out.println("[SKYTOPAUTOMIZER] Hide AutoReply message: " + event.getMessage().getString() );
-        event.setCanceled(true);
-      }
-    } else if (welcomeMatcher.find()) {
-      AutoWelcomer.handleMessage(welcomeMatcher.group(1));
-    } else if (matcher.find()) { 
-      String rank = matcher.group(2);
-      String userName = matcher.group(4).replace(" ", "");
-      String chatMessage = matcher.group(5);
-      String loopNick;
-      Matcher nickMatcher;
-
-      // Replace all nicknames with "<nick>"
-      for (PlayerInfo s : Minecraft.getInstance().getConnection().getOnlinePlayers()) {
-        if (s.getTabListDisplayName() != null)
-          nickMatcher = nickPattern.matcher( s.getTabListDisplayName().getString() );
-        if (nickMatcher.find()) {
-          loopNick = nickMatcher.group(4).replace(" ", "");
-          chatMessage = chatMessage.replaceAll("(?i)" + loopNick, "<nick>");
-        }
-      }
-
-      // Test chat message
-      if (chatMessage.contains("lol")) {
-        System.out.println("[SKYTOPAUTOMIZER] Test auto reply!");
-        Minecraft.getInstance().player.chat("/a " + userName + " kocha torwika <3!");
-      }
-
-      AntyCaps.handleMessage(userName, chatMessage);
-      
-      if (chatMessage.startsWith(".")) {
-        if (rank.equals("HELPER") || rank.equals("MOD") || rank.equals("ADMIN") || rank.equals("WŁAŚCICIEL") || rank.equals("W?A?CICIEL") || userName.equals("Norbiros")) {
-          ChatComplete.handleMessage(chatMessage.replace(".", ""));
-        }
-      }
-    }
+    String s = Minecraft.getInstance().level.getScoreboard().getDisplaySlotName(0);
+    System.out.println(s);
   }
 }
